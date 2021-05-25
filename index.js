98dd316d2cda7ce1df3fd8119d7155e2473bf38b
Sanji-Labz123 <labita0217@gmail.com>
2021-05-25 11:17:43 -0800

    First Commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+
+app.use(express.static('public'));
+
+app.get('/',function(req,res){
+    res.sendFile(path.join(_dirname+'/public/index.html'));
+});
+
+app.get('/aboutme',function(req,res){
+    res.sendFile(path.join(_dirname+'/public/aboutme.html'));
+});
+
+app.get('/contact',function(req,res){
+    res.sendFile(path.join(_dirname+'/public/contact.html'));
+});
+
+app.listen(process.env.PORT || 3000);
+console.log('working on 3000');
